f0b6bcf0b6145ab6f86e94199efc0ee152736829
MSMARGARA <142249109+MSMARGARA@users.noreply.github.com>
2024-02-02 01:41:46 +0000

    user endpoints

--- src/api/routes.py
+++ src/api/routes.py
@@ -5,18 +5,50 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+from flask_jwt_extended import JWTManager, create_access_token
 
 api = Blueprint('api', __name__)
 
 # Allow CORS requests to this API
 CORS(api)
 
+#Endpoint para crear token
+@api.route("/token", methods=["POST"])
+def create_token():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+    user = User.query.filter_by(email=email, password=password).first()
+    if user is None:
+        return jsonify({"msg": "Usuario o contraseña incorrectos"}), 401  
+    access_token = create_access_token(identity=user.id)
+    return jsonify({ "token": access_token, "user_id": user.id })
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+#Endpoint para crear un nuevo usuario
+@api.route("/signup", methods=["POST"])
+def signup_user():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
 
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
+    existing_user = User.query.filter_by(email=email).first()
+    if existing_user:
+        return jsonify({"msg": "El usuario ya existe"}), 400
 
-    return jsonify(response_body), 200
+    new_user = User(email=email, password=password, is_active=True)
+    db.session.add(new_user)
+    db.session.commit()
+
+    access_token = create_access_token(identity=new_user.id)
+    return jsonify({"token": access_token, "user_id": new_user.id}), 201
+
+#Endpoint para autenticar y obtener un token
+@api.route("/login", methods=["POST"])
+def login_user():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+
+    user = User.query.filter_by(email=email, password=password).first()
+    if user is None:
+        return jsonify({"msg": "Usuario o contraseña incorrectos"}), 401
+
+    access_token = create_access_token(identity=user.id)
+    return jsonify({"token": access_token, "user_id": user.id}), 200
